7eef35b7a992c3f25f326bb4bc27b1122aa1d038
KarakusAlican <tr.alikarakus2008@gmail.com>
2024-06-27 13:30:31 +0300

    initial commit

---
+++ src/main/java/com/alican/baklavaDilimi/BaklavaDilimiApplication.java
@@ -0,0 +1,13 @@
+package com.alican.baklavaDilimi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaklavaDilimiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaklavaDilimiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alican/baklavaDilimi/BaklavaDilimiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alican.baklavaDilimi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaklavaDilimiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
