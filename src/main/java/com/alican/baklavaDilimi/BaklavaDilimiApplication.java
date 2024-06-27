ef869d945a35969000918989074392fb1625d6d5
KarakusAlican <tr.alikarakus2008@gmail.com>
2024-06-27 13:34:00 +0300

    baklava dilimi bitti

--- src/main/java/com/alican/baklavaDilimi/BaklavaDilimiApplication.java
+++ src/main/java/com/alican/baklavaDilimi/BaklavaDilimiApplication.java
@@ -8,6 +8,47 @@ public class BaklavaDilimiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BaklavaDilimiApplication.class, args);
+
+
+		int sayi=11;
+		for (int i =1;i<=sayi;i++){
+			for (int j = sayi; j > i; j--) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < i; j++) {
+				System.out.print("*"+" ");
+			}
+			System.out.println("");
+		}
+
+
+
+		for (int i = 1;i<=sayi ; i++) {
+			for (int j =0;j<i; j++) {
+				System.out.print(" ");
+
+			}
+			for (int b =sayi;b>i ; b--) {
+				System.out.print("*"+" ");
+
+			}
+			System.out.println("");
+
+
+		}
+
+/**        for (int i =sayi;0<sayi;i--){
+ for (int j = sayi; 0<j; j--) {
+ System.out.print("*");
+ }
+ for (int j = 0; j < i; j++) {
+ System.out.print("*"+" ");
+ }
+ System.out.println("");
+ }
+ **/
+
+
 	}
 
 }
